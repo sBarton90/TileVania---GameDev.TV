d50f91828967b4fc75d114faaa9f96b86fa52855
Shane <sbarton9@my.centennialcollege.ca>
2021-12-02 14:19:21 -0500

    Added enemy, physics mat, etc

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    [SerializeField] float moveSpeed = 1f;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing() {
+        transform.localScale = new Vector2(Mathf.Sign(moveSpeed), 1f);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,26 +11,39 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-    CapsuleCollider2D myCapsuleCollider;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
     Vector2 moveInput;
+    float startingGravityScale;
+    bool isAlive = true;
 
     void Start() {
         myRigidbody = GetComponent<Rigidbody2D>(); // cache a ref to our own rigidbody.
         myAnimator = GetComponent<Animator>(); // cache a ref to our own animator.
-        myCapsuleCollider = GetComponent<CapsuleCollider2D>(); // cache a ref to our collider.
+        myBodyCollider = GetComponent<CapsuleCollider2D>(); // cache a ref to our collider.
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        startingGravityScale = myRigidbody.gravityScale;
     }
 
     void FixedUpdate() {
+        if (!isAlive) {
+            return;
+        }
         MovePlayer();
         FlipSprite();
         ClimbLadder();
+        Die();
     }
 
     void OnMove(InputValue value) { // when move key is pressed {
+        if (!isAlive) {
+            return;
+        }
         moveInput = value.Get<Vector2>(); // set the value of keypress into moveInput var.
     }
 
     void MovePlayer() {
+
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
 
@@ -38,11 +51,15 @@ public class PlayerMovement : MonoBehaviour
         // ^^ true/false if our velocity is greater than 0
 
         myAnimator.SetBool("isRunning", playerHasHorizontalSpeed); 
-        // ^^ Set the boolean of our animator to the t/f value of our boolean being greater than 0
+         // ^^ Set the boolean of our animator to the t/f value of our boolean being greater than 0
     }
 
     void OnJump(InputValue value) {
-        if (value.isPressed && myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+        if (!isAlive) {
+            return;
+        }
+
+        if (value.isPressed && myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
         }
     }
@@ -57,10 +74,23 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void ClimbLadder() {
-        if (myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
-            Vector2 climbingVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
-            myRigidbody.velocity = climbingVelocity;
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) { //if not touching the climbing layer
+            myRigidbody.gravityScale = startingGravityScale; //gravity is normal, exit this method
+            return;
         }
-        
+        // otherwise, touching is true, so run this code
+        Vector2 climbVelocty = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed); //set a new vec2 for climbing speed
+        myRigidbody.velocity = climbVelocty; // apply vec2 to rigidbody
+        myRigidbody.gravityScale = 0f; //gravity is 0 so we don't fall off ladder
+
+        //when climbing ladder, use climbing animation
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    }
+
+    void Die() {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies"))) {
+            isAlive = false;
+        } 
     }
 }
